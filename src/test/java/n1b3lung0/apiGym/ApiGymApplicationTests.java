d625ed7c7026e616ed1d00363a20cd7112eaeec6
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2023-12-21 18:16:22 +0100

    Initial commit

---
+++ src/test/java/n1b3lung0/apiGym/ApiGymApplicationTests.java
@@ -0,0 +1,13 @@
+package n1b3lung0.apiGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
