f147632a4e580a0af2c56707ef8e536985ec8153
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-01-18 11:12:16 +0100

    Tests for ExerciseDeleter and ExerciseFinder

---
+++ src/test/java/n1b3lung0/apiGym/exercise/application/delete/ExerciseDeleterTests.java
@@ -0,0 +1,37 @@
+package n1b3lung0.apiGym.exercise.application.delete;
+
+import n1b3lung0.apiGym.common.BaseUnitTest;
+import n1b3lung0.apiGym.exercise.application.find.ExerciseFinder;
+import n1b3lung0.apiGym.exercise.domain.ExerciseRepository;
+import n1b3lung0.apiGym.mother.exercise.ExerciseMother;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = ExerciseDeleter.class)
+class ExerciseDeleterTests extends BaseUnitTest {
+
+    @Autowired
+    private ExerciseDeleter deleter;
+
+    @MockBean
+    private ExerciseFinder finder;
+
+    @MockBean
+    private ExerciseRepository repository;
+
+    @Test
+    void shouldMarkExerciseAsDeleted() {
+
+        var exercise = ExerciseMother.random().build();
+        var deleted = exercise.delete();
+
+        when(finder.findById(String.valueOf(exercise.getId()))).thenReturn(exercise);
+        when(repository.save(deleted)).thenReturn(deleted);
+
+        deleter.delete(String.valueOf(exercise.getId()));
+    }
+}
---
+++ src/test/java/n1b3lung0/apiGym/exercise/application/find/ExerciseFinderTests.java
@@ -0,0 +1,64 @@
+package n1b3lung0.apiGym.exercise.application.find;
+
+import n1b3lung0.apiGym.common.BaseUnitTest;
+import n1b3lung0.apiGym.common.application.utils.exception.ExceptionConstants;
+import n1b3lung0.apiGym.exercise.domain.ExerciseRepository;
+import n1b3lung0.apiGym.exercise.domain.exception.ExerciseNotFound;
+import n1b3lung0.apiGym.mother.exercise.ExerciseMother;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = ExerciseFinder.class)
+class ExerciseFinderTests extends BaseUnitTest {
+
+    @Autowired
+    private ExerciseFinder finder;
+
+    @MockBean
+    private ExerciseRepository repository;
+
+    @Test
+    void shouldFindAnExerciseByIdIfNotDeleted() {
+
+        var expected = ExerciseMother.random().build();
+
+        when(repository.findByIdAndDeletedFalse(expected.getId())).thenReturn(Optional.of(expected));
+
+        var actual = finder.findById(String.valueOf(expected.getId()));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFailIfIdNotFound() {
+
+        var id = UUID.randomUUID();
+
+        when(repository.findByIdAndDeletedFalse(id)).thenReturn(Optional.empty());
+
+        var actual = assertThrows(ExerciseNotFound.class, () -> finder.findById(String.valueOf(id)));
+
+        assertEquals(String.format(ExceptionConstants.EXERCISE_NOT_FOUND, id), actual.getMessage());
+    }
+
+    @Test
+    void shouldFailIfExerciseIsDeleted() {
+
+        var exercise = ExerciseMother.random().deleted(Boolean.TRUE).build();
+
+        when(repository.findByIdAndDeletedFalse(exercise.getId())).thenReturn(Optional.empty());
+
+        var actual = assertThrows(ExerciseNotFound.class, () -> finder.findById(String.valueOf(exercise.getId())));
+
+        assertEquals(String.format(ExceptionConstants.EXERCISE_NOT_FOUND, exercise.getId()), actual.getMessage());
+    }
+}
