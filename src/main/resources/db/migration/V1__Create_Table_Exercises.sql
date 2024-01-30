04a68170a079476058a32412ef90639af5efcdcb
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-01-30 17:42:45 +0100

    Add changes to db resources

--- src/main/resources/db/migration/V1__Create_Table_Exercises.sql
+++ src/main/resources/db/migration/V1__Create_Table_Exercises.sql
@@ -1,15 +1,17 @@
 create EXTENSION if not exists "uuid-ossp";
+
 create table exercises (
-    deleted boolean,
-    created_at timestamp(6),
-    updated_at timestamp(6),
-    id uuid not null,
-    created_by varchar(255),
-    description varchar(255),
-    image varchar(255),
-    name varchar(255) not null unique,
-    rest_time varchar(255),
-    updated_by varchar(255),
-    video varchar(255),
-    primary key (id)
+   id uuid not null,
+   name varchar(255) not null unique,
+   description varchar(255),
+   image varchar(255),
+   video varchar(255),
+   rest_time varchar(255),
+   intensity integer,
+   deleted boolean,
+   created_at timestamp(6),
+   created_by varchar(255),
+   updated_at timestamp(6),
+   updated_by varchar(255),
+   primary key (id)
 );
