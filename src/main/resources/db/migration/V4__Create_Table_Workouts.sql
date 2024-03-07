7d21038365613b186b707dae6e1248d6ca03fa2f
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-03-07 19:43:07 +0100

    Update migration

--- src/main/resources/db/migration/V4__Create_Table_Workouts.sql
+++ src/main/resources/db/migration/V4__Create_Table_Workouts.sql
@@ -1,7 +1,7 @@
 create table workouts (
     id uuid not null,
-    end_workout timestamp(6) with time zone,
     start_workout timestamp(6) with time zone,
+    end_workout timestamp(6) with time zone,
     created_at timestamp(6) with time zone,
     created_by varchar(255),
     updated_at timestamp(6) with time zone,
