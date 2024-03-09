27fdde0edc3d647ea365b95cde01664e850a6961
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-03-09 17:46:11 +0100

    Create a workout

--- src/main/java/n1b3lung0/apiGym/workout/application/create/WorkoutCreateRequest.java
+++ src/main/java/n1b3lung0/apiGym/workout/application/create/WorkoutCreateRequest.java
@@ -1,4 +1,47 @@
 package n1b3lung0.apiGym.workout.application.create;
 
-public class WorkoutCreateRequest {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import n1b3lung0.apiGym.exercise_series.domain.ExerciseSeries;
+import n1b3lung0.apiGym.workout.domain.Workout;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Workout create request")
+public final class WorkoutCreateRequest {
+
+    @Schema(description = "List of exercises with their series")
+    private List<ExerciseSeries> exerciseSeries;
+
+    @Schema(description = "Workout Start Time")
+    private ZonedDateTime startWorkout;
+
+    @Schema(description = "Workout End Time")
+    private ZonedDateTime endWorkout;
+
+    public Workout toWorkout(
+            List<ExerciseSeries> exerciseSeries,
+            ZonedDateTime startWorkout,
+            ZonedDateTime endWorkout
+    ) {
+        return Workout.create(
+                exerciseSeries,
+                startWorkout,
+                endWorkout
+        );
+    }
+
+    public static WorkoutCreateRequest fromWorkout(Workout workout) {
+        return new WorkoutCreateRequest(
+                workout.getExerciseSeries(),
+                workout.getStartWorkout(),
+                workout.getEndWorkout()
+        );
+    }
 }
--- src/main/java/n1b3lung0/apiGym/workout/application/create/WorkoutCreator.java
+++ src/main/java/n1b3lung0/apiGym/workout/application/create/WorkoutCreator.java
@@ -1,7 +1,32 @@
 package n1b3lung0.apiGym.workout.application.create;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import n1b3lung0.apiGym.common.application.utils.log.LogConstants;
+import n1b3lung0.apiGym.workout.domain.Workout;
+import n1b3lung0.apiGym.workout.domain.WorkoutRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class WorkoutCreator {
+
+    private final WorkoutRepository repository;
+
+    @Transactional
+    public Workout create(WorkoutCreateRequest request) {
+
+        Workout created = repository.save(request.toWorkout(
+                request.getExerciseSeries(),
+                request.getStartWorkout(),
+                request.getEndWorkout()
+                )
+        );
+
+        log.debug(String.format(LogConstants.WORKOUT_CREATED, created));
+
+        return created;
+    }
 }
