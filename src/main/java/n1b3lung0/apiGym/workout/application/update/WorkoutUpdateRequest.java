7384ef8534c86a670bacb27e45d2a314210b45d8
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-03-09 18:27:30 +0100

    Add Schema annotation to pass the tests again

--- src/main/java/n1b3lung0/apiGym/workout/application/update/WorkoutUpdateRequest.java
+++ src/main/java/n1b3lung0/apiGym/workout/application/update/WorkoutUpdateRequest.java
@@ -1,4 +1,7 @@
 package n1b3lung0.apiGym.workout.application.update;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Workout update request")
 public class WorkoutUpdateRequest {
 }
