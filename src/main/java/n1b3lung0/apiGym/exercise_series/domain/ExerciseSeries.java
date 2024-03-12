6b7a5656a6e21e49132567bc9a3cea5247c0f769
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-03-12 13:18:38 +0100

    Add JoinColumn to workout field

--- src/main/java/n1b3lung0/apiGym/exercise_series/domain/ExerciseSeries.java
+++ src/main/java/n1b3lung0/apiGym/exercise_series/domain/ExerciseSeries.java
@@ -5,14 +5,17 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import lombok.With;
 import n1b3lung0.apiGym.common.domain.audit.AuditFields;
 import n1b3lung0.apiGym.exercise.domain.Exercise;
@@ -25,42 +28,38 @@ import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@Entity
-@Data
-@Builder
-@RequiredArgsConstructor
-@NoArgsConstructor(force = true)
-@Table(name = "exercise_series")
+@Entity @Table(name = "exercise_series")
+@Data @Builder @RequiredArgsConstructor @NoArgsConstructor(force = true)
 public final class ExerciseSeries implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -1L;
 
-    @With
-    @Id @GeneratedValue
+    @With @Id @GeneratedValue
     private final UUID id;
 
-    @ManyToOne
+    @With @ManyToOne
+    @JoinColumn(name = "workout_id", nullable = false)
     private final Workout workout;
 
     @OneToOne
     private final Exercise exercise;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @OneToMany(mappedBy = "exerciseSeries")
     private final List<Series> series;
 
+    @With @Column(name = "weight")
     private final Float weight;
 
-    @With
-    @Column(name = "start_series")
+    @With @Column(name = "start_series")
     private final ZonedDateTime startSeries;
 
-    @With
-    @Column(name = "end_series")
+    @With @Column(name = "end_series")
     private final ZonedDateTime endSeries;
 
-    @With
-    @Column(name = "rest_time")
+    @With @Column(name = "rest_time")
     private final RestTime restTime;
 
     @Embedded
